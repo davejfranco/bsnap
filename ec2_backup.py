575bac3c418314a5521585ca7480693ce71533c9
Dave Franco <dave.franco@blanclink.com>
2014-10-17 11:39:11 -0430

    add function to delete old snapshots

--- ec2_backup.py
+++ ec2_backup.py
@@ -9,44 +9,41 @@ bsnap module:
 Basically creates snapshots of attached volumes,
 checks if there is any old snapshots and deletes it.
 """
+
 import boto.ec2
 from datetime import datetime, timedelta
 
-print '***********************'
-print '*  Making Backup      *'
-print '***********************'
+print('***********************')
+print('*  Making Backup      *')
+print('***********************')
+
 
 #Connecting to us_west-region
-today = date.today())
+ec2 = boto.ec2.connect_to_region('us-west-2')
+Snapshots = ec2.get_all_snapshots(filters={'tag:Name':'snap_*'})
+
+#Delete old Snapshots
+def old_snapshots():
+
+    delete_time = datetime.utcnow() - timedelta(14)
+    for snapshot in Snapshots:
+        start_time = datetime.strptime(snapshot.start_time,'%Y-%m-%dT%H:%M:%S.000Z')
+        if start_time < delete_time:
+            snapshot.delete()
+        print('Done')
 
 
-#def old_snapshots():
-
-#	snapshots = ec2.get_all_snapshots(filters={'tag:Creation_date':'*'}
-
-#	return snapshots
-
-
-
-def get_ec2_volumes(tag_key, tag_value):
-
-	filter = {
-		'tag:'+tag_key:tag_value
-	}
-	volumes = ec2.get_all_volumes(filters=filter)
-	return volumes
- 
 def Backup_volumes():
 	
-	volumes = get_ec2_volumes('Name', 'qa-www')
+	volumes=ec2.get_all_volumes(filters={'tag:Name':''})
 	for n in range(len(volumes)):
-		if volumes[n].attachment_state() == 'attached':
+		if volumes[n].attachment_state()=='attached':
 			for v in volumes:
 				v.create_snapshot('Weekly_Backup')
-	
-	print 'Listo'
 
-Backup_volumes()
+	print ('Listo')
 
+#Backup_volumes()
+old_snapshots()
 
 
