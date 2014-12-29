055a5681e5e063ef938fab87eb04c95c1df33860
dave.franco <dave.franco@blanclink.com>
2014-12-29 14:56:58 -0430

    add sns notification, and some style programming changes

--- bsnap/app.py
+++ bsnap/app.py
@@ -12,10 +12,15 @@ checks if there are any old snapshots and deletes them.
 
 import sys
 import boto.ec2
-from . import log
+import boto.sns
+import log
 from datetime import datetime, timedelta
 
-def delete_old_snapshots(ec2):
+# Connecting to us_west-region
+ec2 = boto.ec2.connect_to_region('us-west-2')
+sns = boto.sns.connect_to_region('us-west-2')
+
+def DeleteSnap(ec2):
     """
     Retrieves all snapshots on the us-west-2
     region and catch the start_time attribute, if is 14 days
@@ -23,7 +28,7 @@ def delete_old_snapshots(ec2):
     """
     Snapshots = ec2.get_all_snapshots(filters={'tag:Name':'*'})
     delete_time = datetime.utcnow() - timedelta(7)
-
+    
     try:
         log.info('Deleting old Snapshots...')
         for snapshot in Snapshots:
@@ -33,11 +38,13 @@ def delete_old_snapshots(ec2):
                 snapshot.delete()
     except:
         log.error('Unable to delete snapshots')
+        return False
     else:
         log.error('Successful snapshot delete')
+        return True
 
 
-def backup_volumes(ec2):
+def BackupVol(ec2):
     """
     Retrieve all the volumes in the us-west-2 region and
     it will create a snapshot only if the volumes is
@@ -51,18 +58,34 @@ def backup_volumes(ec2):
                 volumes[n].create_snapshot('Weekly Backup')
     except:
         log.error('Unable to create snapshots')
+        return False
     else:
         log.info('Successful snapshot creation')
+        return True
 
+def SnsNotify(sns):
+    """
+    Retrieve the return value from delete_old_snapshots
+    and backup_volumes and send notification via sns to
+    a specific topic
+    """
+    topic = "arn:aws:sns:us-west-2:816295349629:testing"
+    subject = "bsnap Backup"
+    msg_win = "Successful bsnap Operation"
+    msg_fail = "Failed bsnap Operation, check syslog entries"
+
+    if DeleteSnap(ec2) == True and BackupVol(ec2) == True:
+        sns.publish(topic, msg_win, subject)
+
+    elif DeleteSnap(ec2) == False and BackupVol(ec2) == False:
+        sns.publish(topic, msg_fail, subject)
 
 def main():
     """Main entry point"""
-    # Connecting to us_west-region
-    ec2 = boto.ec2.connect_to_region('us-west-2')
-    delete_old_snapshots(ec2)
-    backup_volumes(ec2)
+    DeleteSnap(ec2)
+    BackupVol(ec2)
+    SnsNotify(sns)
     return 0
 
 if __name__=='__main__':
     sys.exit(main())
-
