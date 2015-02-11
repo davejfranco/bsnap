a4d026927f48b41da67731df1332341eac6208ed
Dave Franco <dave.franco@blanclink.com>
2015-02-11 15:10:00 -0430

    Bug fixed and update README

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
-## **bsnap** ##
 
-Deletes 14 days old snapshots an creates new ones from attached volumes.
+##**BSNAP**###
+
+Deletes 7 days old snapshots and it creates new ones from 'in-use' volumes.
+It will notify via aws sns whether if the process success or failed
+
 
 ## **Requirements:** ##
 
- - python3
+ - python2.7
  - Boto >= 2.33.0
  - Virtualenv
 
 ## **Usage:** ##
 
 Install the script using 'python setup.py install',
-create a cron weekly job pointing to the main file.
+create a cron job pointing to the main file.
+
+For security reasons the script doesn't provide any 
+credential information, instead you should either
+have the key in a config file or installing the script in a
+instance with an IAM role. 
--- bsnap/app.py
+++ bsnap/app.py
@@ -23,7 +23,7 @@ sns = boto.sns.connect_to_region('us-west-2')
 def DeleteSnap(ec2):
     """
     Retrieves all snapshots on the us-west-2
-    region and catch the start_time attribute, if is 14 days
+    region and catch the start_time attribute, if is 7 days
     older than the actual date are going to be deleted.
     """
 
@@ -31,19 +31,17 @@ def DeleteSnap(ec2):
     delete_time = datetime.utcnow() - timedelta(7)
     
     try:
-        
         log.info('Deleting old Snapshots...')
         for snapshot in Snapshots:
             start_time = datetime.strptime(snapshot.start_time,
                                        '%Y-%m-%dT%H:%M:%S.000Z')
             if start_time < delete_time:
-                snapshot.delete()
-        
+                snapshot.delete(dry_run=True)
     except:
         log.error('Unable to delete snapshots')
         return False
     else:
-        log.error('Successful snapshot delete')
+        log.info('Successful snapshot delete')
         return True
     
 def BackupVol(ec2):
@@ -54,12 +52,11 @@ def BackupVol(ec2):
     """
     
     try:
-        
         log.info('Creating Snapshots...')
         volumes=ec2.get_all_volumes()
-        for n in range(len(volumes)):
-            if volumes[n].attachment_state()=='attached':
-                volumes[n].create_snapshot('Weekly Backup')
+        for v in volumes:
+            if v.status()=='in-use':
+                v.create_snapshot(dry_run=True)
     except:
         log.error('Unable to create snapshots')
         return False
@@ -67,13 +64,9 @@ def BackupVol(ec2):
         log.info('Successful snapshot creation')
         return True
     
-def SnsNotify(sns):
-    """
-    Retrieve the return value from delete_old_snapshots
-    and backup_volumes and send notification via sns to
-    a specific topic
-    """
-    topic = "arn:aws:sns:us-west-2:816295349629:notifySysadmin"
+def main():
+    """Main entry point"""
+    topic = arn_topic
     subject = "bsnap Backup"
     msg_win = "Successful bsnap Operation"
     msg_fail = "Failed bsnap Operation, check syslog entries"
@@ -81,15 +74,8 @@ def SnsNotify(sns):
     if DeleteSnap(ec2) == True and BackupVol(ec2) == True:
         sns.publish(topic, msg_win, subject)
 
-    elif DeleteSnap(ec2) == False and BackupVol(ec2) == False:
+    else:
         sns.publish(topic, msg_fail, subject)
-
-def main():
-    """Main entry point"""
-    DeleteSnap(ec2)
-    BackupVol(ec2)
-    SnsNotify(sns)
-    return 0
 
 if __name__=='__main__':
     sys.exit(main())
